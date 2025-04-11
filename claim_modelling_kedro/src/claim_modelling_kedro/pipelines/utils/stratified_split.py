4f5010dfe56fa01f235a01df049dbd8b486e80ab
Krzysztof Piesiewicz <krz.piesiewicz@gmail.com>
2025-04-11 19:24:43 +0200

    stratified sampling - implemented more efficient version

--- claim_modelling_kedro/src/claim_modelling_kedro/pipelines/utils/stratified_split.py
+++ claim_modelling_kedro/src/claim_modelling_kedro/pipelines/utils/stratified_split.py
@@ -18,48 +18,39 @@ def get_stratified_sample_keys(target_df: pd.DataFrame, stratify_target_col: str
         size (Union[int, float]): The number of buckets (int) or fraction of the dataset (float) to sample.
         shuffle (bool): Whether to shuffle the data within each bucket before sampling. Default is True.
         random_seed (int): The random seed for reproducibility. Default is 0.
-        verbose (bool): Whether to print information about the sampling process. Default is True.
+        verbose (bool): Whether to log information about the sampling process. Default is True.
 
     Returns:
         pd.Index: A Index containing the stratified sample keys.
     """
     # Ensure size is valid
+    n_samples = target_df.shape[0]
     if isinstance(size, float):
         if not (0 < size <= 1):
             raise ValueError("If size is a float, it must be in the range (0, 1]. But got {size=}.")
-        n_buckets = int(target_df.shape[0] * size)
+        n_buckets = int(n_samples * size)
     elif isinstance(size, int):
-        if size <= 0 or size > target_df.shape[0]:
+        if size <= 0 or size > n_samples:
             raise ValueError(
-                f"If size is an int, it must be in the range from 1 to {target_df.shape[0]=}. But got {size=}.")
+                f"If size is an int, it must be in the range from 1 to {n_samples=}. But got {size=}.")
         n_buckets = size
     else:
         raise TypeError(f"Size must be either an int or a float. But got {size=} of type {type(size)}.")
 
     # Sort the DataFrame by the stratify_target_col
-    sorted_df = target_df.sort_values(by=stratify_target_col, ascending=True)
+    sorted_indices = target_df.sort_values(by=stratify_target_col, ascending=False).index.values
 
-    # Divide the sorted DataFrame into buckets
-    bucket_size = len(sorted_df) // n_buckets
-    buckets = [sorted_df.iloc[i * bucket_size:(i + 1) * bucket_size] for i in range(n_buckets)]
+    buckets_beginings = np.linspace(0, n_samples, n_buckets + 1).astype(int)
 
-    # Handle any remaining samples (last bucket may be larger)
-    if len(sorted_df) % n_buckets != 0:
-        buckets[-1] = pd.concat([buckets[-1], sorted_df.iloc[n_buckets * bucket_size:]])
+    # Optional: Shuffle the indicies within a bucket (if shuffle is enabled)
+    if shuffle:
+        rng = np.random.default_rng(random_seed)
+        for left, right in zip(buckets_beginings[:-1], buckets_beginings[1:]):
+            rng.shuffle(sorted_indices[left:right])
 
     # Sample one observation from each bucket
-    sampled_indices = []
-    rng = np.random.default_rng(random_seed)
-    for bucket in buckets:
-        if shuffle:
-            sampled_indices.append(rng.choice(bucket.index, size=1, replace=False)[0])
-        else:
-            sampled_indices.append(bucket.index[0])  # Take the first observation if no shuffle
-
-    if verbose:
-        print(f"Stratified sampling completed. {len(sampled_indices)} samples selected from {n_buckets} buckets.")
-
-    return sorted_df.loc[sampled_indices, :].index
+    sampled_indices = sorted_indices[buckets_beginings[:-1]]
+    return target_df.loc[sampled_indices, :].index
 
 
 def get_stratified_train_calib_test_split_keys(target_df: pd.DataFrame, stratify_target_col: str,
@@ -76,7 +67,7 @@ def get_stratified_train_calib_test_split_keys(target_df: pd.DataFrame, stratify
         test_size (Union[int, float]): The size of the test set (int for absolute, float for fraction).
         shuffle (bool): Whether to shuffle the data before sampling. Default is True.
         random_seed (int): The random seed for reproducibility. Default is 0.
-        verbose (bool): Whether to print information about the splits. Default is True.
+        verbose (bool): Whether to log information about the splits. Default is True.
         calib_size (Union[int, float], optional): The size of the calibration set (int for absolute, float for fraction).
 
     Returns:
@@ -118,7 +109,7 @@ def get_stratified_train_calib_test_split_keys(target_df: pd.DataFrame, stratify
     train_keys = remaining_df.index
 
     if verbose:
-        print(
+        logger.info(
             f"Split completed: {len(train_keys)} train, {len(calib_keys) if calib_keys is not None else 0} calibration, {len(test_keys)} test samples.")
 
     if verbose:
