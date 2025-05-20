ad7e15483589ff20cd285a913b65668eb89f02bc
Krzysztof Piesiewicz <krz.piesiewicz@gmail.com>
2025-05-20 16:27:14 +0200

    cc_index.py: bugfix in sorting

--- claim_modelling_kedro/src/claim_modelling_kedro/pipelines/utils/metrics/cc_index.py
+++ claim_modelling_kedro/src/claim_modelling_kedro/pipelines/utils/metrics/cc_index.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 from claim_modelling_kedro.pipelines.p01_init.config import Config
 from claim_modelling_kedro.pipelines.p01_init.metric_config import MetricEnum, MetricType
@@ -10,45 +11,53 @@ class NormalizedConcentrationIndex(Metric):
         super().__init__(config, sklearn_like_metric=self._weighted_normalized_concentration_index, **kwargs)
 
     @staticmethod
-    def _weighted_concentration_index(y_true: np.ndarray, y_pred: np.ndarray, sample_weight: np.ndarray = None):
+    def _weighted_concentration_index(y_true: pd.Series, y_pred: pd.Series, sample_weight: pd.Series = None):
         """
         Compute the weighted Concentration Index.
 
         Args:
-            y_true (np.ndarray): Array of true values.
-            y_pred (np.ndarray): Array of predicted values.
-            sample_weight (np.ndarray): Array of sample weights. If None, all weights are set to 1.
+            y_true (pd.Series): Series of true values.
+            y_pred (pd.Series): Series of predicted values.
+            sample_weight (pd.Series): Series of sample weights. If None, all weights are set to 1.
 
         Returns:
             float: Weighted Concentration Index.
         """
         assert len(y_true) == len(y_pred), "y_true and y_pred must have the same length"
+
+        y_true = pd.Series(y_true)
+        y_pred = pd.Series(y_pred)
         if sample_weight is None:
-            sample_weight = np.ones_like(y_true)
+            sample_weight = pd.Series(np.ones_like(y_true), index=y_true.index)
+        else:
+            sample_weight = pd.Series(sample_weight)
 
-        # Combine all data into a single array
-        data = np.asarray(np.c_[y_true, y_pred, sample_weight], dtype=float)
+        # Sort inputs stably by y_pred, then by hash(index)
+        secondary_key = y_pred.index.to_series().apply(lambda x: hash(x)).values
+        primary_key = y_pred.values
+        sorted_idx = np.lexsort((secondary_key, primary_key))
 
-        # Sort by predicted values and ensure stable sorting
-        data_sorted = data[np.argsort(data[:, 1], kind="stable")]
+        y_true = y_true.iloc[sorted_idx].reset_index(drop=True)
+        y_pred = y_pred.iloc[sorted_idx].reset_index(drop=True)
+        sample_weight = sample_weight.iloc[sorted_idx].reset_index(drop=True)
 
         # Compute cumulative sums
-        cum_true = np.cumsum(data_sorted[:, 0] * data_sorted[:, 2])  # cumulative sums of true * weight
-        cum_weight = np.cumsum(data_sorted[:, 2])  # cumulative sums of weights
-        total_true = np.sum(data_sorted[:, 0] * data_sorted[:, 2])  # sum of all true * weight
-        sum_of_weights = cum_weight[-1]
+        cum_true = np.cumsum(y_true * sample_weight)  # cumulative sums of true * weight
+        cum_weight = np.cumsum(sample_weight)  # cumulative sums of weights
+        total_true = np.sum(y_true * sample_weight)  # sum of all true * weight
+        sum_of_weights = cum_weight.iloc[-1]
 
         return 1 / 2 - np.sum(cum_true) / total_true / sum_of_weights
 
     @staticmethod
-    def _weighted_normalized_concentration_index(y_true: np.ndarray, y_pred: np.ndarray, sample_weight: np.ndarray = None):
+    def _weighted_normalized_concentration_index(y_true: pd.Series, y_pred: pd.Series, sample_weight: pd.Series = None):
         """
         Compute the normalized weighted Concentration Index.
 
         Args:
-            y_true (np.ndarray): Array of true values.
-            y_pred (np.ndarray): Array of predicted values.
-            sample_weight (np.ndarray): Array of sample weights. If None, all weights are set to 1.
+            y_true (pd.Series): Series of true values.
+            y_pred (pd.Series): Series of predicted values.
+            sample_weight (pd.Series): Series of sample weights. If None, all weights are set to 1.
 
         Returns:
             float: Normalized weighted Concentration Index.
@@ -57,7 +66,7 @@ class NormalizedConcentrationIndex(Metric):
         concentration_index_true = NormalizedConcentrationIndex._weighted_concentration_index(y_true, y_true, sample_weight)
 
         # Avoid division by zero
-        if concentration_index_true == 0:
+        if np.isclose(concentration_index_true, 0, atol=1e-5):
             return 0.0
 
         return concentration_index_pred / concentration_index_true
