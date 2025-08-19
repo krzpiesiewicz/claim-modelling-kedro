0dd8656fc57996d72d6b172396b149dce64a36c5
Krzysztof Piesiewicz <krz.piesiewicz@gmail.com>
2025-08-19 23:10:29 +0200

    refactor: update PowerTargetTransformer to include y_shift parameter in transformation

--- claim_modelling_kedro/src/claim_modelling_kedro/pipelines/p07_data_science/target_transformers/power_transformer.py
+++ claim_modelling_kedro/src/claim_modelling_kedro/pipelines/p07_data_science/target_transformers/power_transformer.py
@@ -8,11 +8,16 @@ from claim_modelling_kedro.pipelines.p07_data_science.transform_target import Ta
 class PowerTargetTransformer(TargetTransformer):
     """
     A transformer that applies a power transformation to the target variable.
-    The transformation is defined as y' = y^(1/power), where power is a parameter specified in the configuration.
-    The inverse transformation is defined as y = (y')^power.
+    The transformation is defined as y' = (y+y_shift)^(1/power),
+    where y is the original target value, y_shift is a constant shift to avoid negative values
+    and power is the exponent used in the transformation.
+    The inverse transformation is defined as y = (y')^power - y_shift.
     """
     def __init__(self, config: Config, target_col: str, pred_col: str = None):
         super().__init__(config=config, target_col=target_col, pred_col=pred_col)
+        self.y_shift = self.params.get("y_shift")
+        if self.y_shift is None:
+            raise ValueError("Parameter y_shift must be specified in the configuration of PowerTargetTransformer.")
         self.power = self.params.get("power")
         if self.power is None:
             raise ValueError("Parameter power must be specified in the configuration of PowerTargetTransformer.")
@@ -22,8 +27,8 @@ class PowerTargetTransformer(TargetTransformer):
         pass
 
     def _transform(self, y_true: pd.Series) -> pd.Series:
-        powered_y = y_true.apply(lambda y: np.power(y, 1 / self.power))
+        powered_y = y_true.apply(lambda y: np.power(y + self.y_shift, 1 / self.power))
         return powered_y
 
     def _inverse_transform(self, y_pred: pd.Series) -> pd.Series:
-        return y_pred.apply(lambda x: np.power(x, self.power))
+        return y_pred.apply(lambda x: np.power(x, self.power) - self.y_shift)
