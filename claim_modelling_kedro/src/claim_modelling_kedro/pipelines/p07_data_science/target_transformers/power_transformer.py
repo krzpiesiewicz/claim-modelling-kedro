d402bfe19cc2f0c2114ed69e834c5af5661fd987
Krzysztof Piesiewicz <krz.piesiewicz@gmail.com>
2025-08-19 23:02:32 +0200

    refactor PowerTargetTransformer + improve documentation

--- claim_modelling_kedro/src/claim_modelling_kedro/pipelines/p07_data_science/target_transformers/power_transformer.py
+++ claim_modelling_kedro/src/claim_modelling_kedro/pipelines/p07_data_science/target_transformers/power_transformer.py
@@ -7,7 +7,9 @@ from claim_modelling_kedro.pipelines.p07_data_science.transform_target import Ta
 
 class PowerTargetTransformer(TargetTransformer):
     """
-    A transformer that applies a logarithmic transformation to the target variable.
+    A transformer that applies a power transformation to the target variable.
+    The transformation is defined as y' = y^(1/power), where power is a parameter specified in the configuration.
+    The inverse transformation is defined as y = (y')^power.
     """
     def __init__(self, config: Config, target_col: str, pred_col: str = None):
         super().__init__(config=config, target_col=target_col, pred_col=pred_col)
@@ -20,8 +22,8 @@ class PowerTargetTransformer(TargetTransformer):
         pass
 
     def _transform(self, y_true: pd.Series) -> pd.Series:
-        sqrt_y = y_true.apply(lambda y: np.power(y, 1 / self.params["power"]))
-        return sqrt_y
+        powered_y = y_true.apply(lambda y: np.power(y, 1 / self.power))
+        return powered_y
 
     def _inverse_transform(self, y_pred: pd.Series) -> pd.Series:
-        return y_pred.apply(lambda x: np.power(x, self.params["power"]))
+        return y_pred.apply(lambda x: np.power(x, self.power))
